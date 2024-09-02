74ea81944500b7c66e99b181a3d7187f083975ed
Xwaleedh <waledhnihal@outlook.com>
2024-09-02 22:39:05 +0530

    Added firebase to JS

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ window.addEventListener("load", () => {
     });
   });
   
+  // Initialize Firebase
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  };
+  
+  
+  // Initialize Firebase App
+  const app = firebase.initializeApp(firebaseConfig);
+  
+  
+  // Initialize Firebase Services
+  const auth = firebase.auth();
+  const db = firebase.firestore();
