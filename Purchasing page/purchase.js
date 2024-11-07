bb0d661c940ac4fca90769cc830de2a950a0bd6d
Xwaleedh <waledhnihal@outlook.com>
2024-11-07 18:08:02 +0530

    removed firebase initialisation

--- Purchasing page/purchase.js
+++ Purchasing page/purchase.js
@@ -74,25 +74,3 @@ function loadProductDetails() {
 
 // Load the product details when the page loads
 window.onload = loadProductDetails;
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCwO0xuY9imGzuoxuoun6XXprB50W6NOfQ",
-  authDomain: "semmandalluwa-test.firebaseapp.com",
-  projectId: "semmandalluwa-test",
-  storageBucket: "semmandalluwa-test.firebasestorage.app",
-  messagingSenderId: "140774806089",
-  appId: "1:140774806089:web:ea67fadc0e3ada8cc5e598",
-  measurementId: "G-84M46V5JQF"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
--- Store/script.js
+++ Store/script.js
@@ -34,25 +34,3 @@ fetch('http://localhost/Semmandalluwa.lk/server/crud.php/items')
     document.getElementById('data').innerHTML = output;
   })
   .catch(err => console.log(err));
-
-  // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCwO0xuY9imGzuoxuoun6XXprB50W6NOfQ",
-  authDomain: "semmandalluwa-test.firebaseapp.com",
-  projectId: "semmandalluwa-test",
-  storageBucket: "semmandalluwa-test.firebasestorage.app",
-  messagingSenderId: "140774806089",
-  appId: "1:140774806089:web:ea67fadc0e3ada8cc5e598",
-  measurementId: "G-84M46V5JQF"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
