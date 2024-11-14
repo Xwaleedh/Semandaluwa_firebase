9b137f15780a4ca5f8bbe91495900b7bff6cf37c
Xwaleedh <waledhnihal@outlook.com>
2024-11-14 14:02:20 +0530

    Fixed sign up error

--- LogIn page/auth.js
+++ LogIn page/auth.js
@@ -1,13 +1,15 @@
-// sign-in.js
+import { getAuth, logi } from 'https://www.gstatic.com/firebasejs/9.6.5/firebase-auth.js';
+import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.6.5/firebase-firestore.js';
+import { auth, db } from '../firebase-init.js';  // Import auth and db from firebase-init.js
 
-document.getElementById('sign-in-form').addEventListener('submit', function (e) {
+document.getElementById('loginForm').addEventListener('submit', function (e) {
   e.preventDefault(); // Prevent the form from submitting the usual way
 
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
   // Sign in the user with email and password
-  firebase.auth().signInWithEmailAndPassword(email, password)
+  log(email, password)
     .then(userCredential => {
       // Signed in successfully
       console.log('User signed in:', userCredential.user);
--- sign up page/sign-up.js
+++ sign up page/sign-up.js
@@ -1,6 +1,6 @@
 import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/9.6.5/firebase-auth.js';
 import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.6.5/firebase-firestore.js';
-import { auth, db } from './firebase-init.js';  // Import auth and db from firebase-init.js
+import { auth, db } from '../firebase-init.js';  // Import auth and db from firebase-init.js
 
 // Handle sign-up form submission
 const signUpForm = document.getElementById('signUpForm');
