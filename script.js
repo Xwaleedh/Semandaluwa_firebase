79ba0260dc9bcf7c23d10930541fb618d3b0a71a
Xwaleedh <waledhnihal@outlook.com>
2024-11-23 18:16:13 +0530

    removed unused code

--- script.js
+++ script.js
@@ -11,14 +11,3 @@ window.addEventListener("load", () => {
       document.body.removeChild(loader);
     });
   });
-  
-  // Initialize Firebase
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
-  
